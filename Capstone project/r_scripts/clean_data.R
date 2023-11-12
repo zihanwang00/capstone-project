0cf9a751edcb46702b2c9452494ba7ecf2b3e3b8
zihanwang00 <113872795+zihanwang00@users.noreply.github.com>
2023-11-11 23:27:56 -0800

    Update clean_data.R

--- Capstone project/r_scripts/clean_data.R
+++ Capstone project/r_scripts/clean_data.R
@@ -2,18 +2,14 @@
 
 clean_data <- function(jobs_df) {
   cleaned_df <- jobs_df %>%
-    select(experience_level,
-           employment_type,
-           job_title,
-           salary_in_usd,
-           employee_residence,
-           remote_ratio) %>%
     filter(
       employment_type == "FT"
     ) %>%
     mutate(
       experience_level = as.factor(experience_level),
       employment_type = as.factor(employment_type), 
+      employee_residence = as.factor(employee_residence),
+      remote_ratio = as.factor(remote_ratio),
       job_title = str_to_lower(job_title), 
       job_title = if_else(str_detect(job_title, "data") & (str_detect(job_title, "scientist") | str_detect(job_title, "science")), "data scientist",
                      if_else(str_detect(job_title, "data") & (str_detect(job_title, "analyst")| str_detect(job_title, "analytics")), "data analyst",
@@ -22,8 +18,14 @@ clean_data <- function(jobs_df) {
                      if_else(str_detect(job_title, "ai"), "AI programmer",
                      if_else(str_detect(job_title, "machine learning") | str_detect(job_title, "ml"),  "machine learning engineer",
                      if_else((str_detect(job_title, "research") | str_detect(job_title, "applied")) & str_detect(job_title, "scientist"), "research scientist",                                                       job_title))))))), 
-      job_title = as.factor(toTitleCase(job_title))
-      )
+      job_title = as.factor(toTitleCase(job_title))) %>%
+    select(
+      experience_level,
+      job_title,
+      salary_in_usd,
+      employee_residence,
+      remote_ratio)
+      
   return(cleaned_df)
 }
 
