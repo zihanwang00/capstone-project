7a52c6abdf4d29ba0dd1cc31039b4c350e8e9fc2
zihanwang00 <113872795+zihanwang00@users.noreply.github.com>
2023-11-12 21:56:54 -0800

    Update fit_model.R

--- Capstone project/r_scripts/fit_model.R
+++ Capstone project/r_scripts/fit_model.R
@@ -1,4 +1,8 @@
-linear_model <- function(data, y) {
-  lm_model <- glm(salary_in_usd ~ ., data = cleaned_jobs)
-  summary(lm_model)
+linear_model <- function(df) {
+  lm_model <- glm(salary_in_usd ~ ., data = df)
+  return(lm_model)
+}
+
+salary_prediction <- function(m, level, title, turn_over, ratio){
+  pre_new <- predict(model, data.frame(experience_level = level, job_title = title, employee_residence = residence, remote_ratio = ratio))
 }
