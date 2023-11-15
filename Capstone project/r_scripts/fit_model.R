b22ad5c43e77eb5003f6cc846b28096686b31fd3
zihanwang00 <113872795+zihanwang00@users.noreply.github.com>
2023-11-14 22:08:45 -0800

    Delete fit_model.R

--- Capstone project/r_scripts/fit_model.R
+++
@@ -1,8 +0,0 @@
-linear_model <- function(df) {
-  lm_model <- glm(salary_in_usd ~ ., data = df)
-  return(lm_model)
-}
-
-salary_prediction <- function(m, level, title, turn_over, ratio){
-  pre_new <- predict(model, data.frame(experience_level = level, job_title = title, employee_residence = residence, remote_ratio = ratio))
-}
